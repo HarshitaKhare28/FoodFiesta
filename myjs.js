5237909aeb62ff4f90f71626f317f8f4313b4473
HarshitaKhare28 <121930563+HarshitaKhare28@users.noreply.github.com>
2023-09-19 21:43:19 +0530

    Add files via upload

---
+++ myjs.js
@@ -0,0 +1,31 @@
+// myjs.js
+document.addEventListener('DOMContentLoaded', function () {
+    const primaryOrderButton = document.querySelector('.button');
+    const secondaryOrderButtons = document.querySelectorAll('.sec-button');
+
+    primaryOrderButton.addEventListener('click', () => {
+        alert('Order Now button clicked from primary section!');
+    });
+
+    secondaryOrderButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            alert('Order Now button clicked from secondary section!');
+        });
+    });
+    const navLinks = document.querySelectorAll('nav ul li a');
+    navLinks.forEach(link => {
+        link.addEventListener('click', function (e) {
+            e.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                window.scrollTo({
+                    top: targetElement.offsetTop,
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
+}
+);
